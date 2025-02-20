e5838e6c4d9a841797427123b6df6703bae1a08f
Harshit Vijay <93333205+Coder-Harshit@users.noreply.github.com>
2025-02-20 18:05:06 +0530

    log console widget added

--- screens/home.py
+++ screens/home.py
@@ -22,11 +22,38 @@ class HomeWidget(QtWidgets.QWidget):
         self.settings_btn = QtWidgets.QPushButton("SETTINGS")
         self.settings_btn.clicked.connect(lambda : print("clicked"))
 
+        self.log_viewer = QtWidgets.QLabel("Logs go here....")
+
+        l3 = QtWidgets.QVBoxLayout()
+        l3.addWidget(self.log_viewer)
+
+        l4 = QtWidgets.QHBoxLayout()
+        self.clear_logs_btn = QtWidgets.QPushButton("Clear Logs")
+        self.clear_logs_btn.clicked.connect(self.empty_logs)
+        self.save_logs_btn = QtWidgets.QPushButton("Save Logs")
+        self.save_logs_btn.clicked.connect(self.save_logs)
+        l4.addWidget(self.clear_logs_btn)
+        l4.addWidget(self.save_logs_btn)
+
+        l3.addLayout(l4)
+
         l2 = QtWidgets.QVBoxLayout()
         l2.addWidget(self.settings_btn)
         l2.addLayout(l1)
-
+        l2.addLayout(l3)
         self.setLayout(l2)        
 
     def search_url(self):
         print(validateURL.validate(self.url_bar.text())) 
+
+    def empty_logs(self):
+        self.log_viewer.setText("Logs go here....")
+
+    def save_logs(self):
+        filename, _ = QtWidgets.QFileDialog.getSaveFileName(self, caption="Save File", dir='logs')
+        print(filename)
+        if filename:
+            with open(filename,'w') as file:
+                print(self.log_viewer.text(),file=file)
+        else: 
+            print("OPERATION TERMINATED")
